3a047ef5ef2a69f810d479e794763bd33cd4952b
matt <matt@xmail.com>
2023-05-10 21:49:00 -0300

    Init the rename-vm.sh script for rename viartual machine. Check this code please!

--- vm_management/scripts/rename-vm.sh
+++ vm_management/scripts/rename-vm.sh
@@ -4,8 +4,29 @@
 # File Name: rename-vm.sh
 # Author: Matias Barboza
 # Date Created: May 10th, 2023
-# Description: This script performs a specific task.
+# Description: Change our virtual machine name.
 # Version: 0.1
 ################################################################################
 
-# Script code starts here...
+
+# Execute the command and store the output in variables
+vm_name=$(virsh list --all | grep $1 | awk '{print $2}')
+
+# Check if the output is equal to '-'
+if [ "$vm_name" = "$1" ]; then
+    
+    output=$(virsh list --all | grep $1 | awk '{print $1}')
+    
+    if [ "$output" = "-" ]; then
+        #echo "The VM is powerred off"
+        mv /etc/libvirt/qemu/"$1".xml /etc/libvirt/qemu/"$2".xml
+    else
+        echo "The VM is running, for rename first turn off"
+    fi
+
+else
+    echo "The requested VM does not exist!"
+fi
+
+
+## END
