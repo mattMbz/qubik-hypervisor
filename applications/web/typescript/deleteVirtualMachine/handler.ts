f26068aed0bd1b8bdfadea54f4f4b21ffb3bd5c7
mattMbz <matias.mbz@gmail.com>
2023-07-11 18:12:55 -0300

    Creating a Virtual machines searcher that we want delete

--- applications/web/typescript/deleteVirtualMachine/handler.ts
+++ applications/web/typescript/deleteVirtualMachine/handler.ts
@@ -6,8 +6,10 @@ export class HandlerToDeleteVM {
     private container: HTMLElement;
     private buttons: NodeListOf<Element>;
     private requestHandler: RequestHandler;
+    private handleToSearching: HandlerToSearching;
 
     constructor(containerSelector: string, buttonSelector: string) {
+        this.handleToSearching = new HandlerToSearching('#searcherInput');
         this.container = document.querySelector(containerSelector) as HTMLElement;
         this.buttons = this.container.querySelectorAll(buttonSelector);
         this.requestHandler = new RequestHandler();
@@ -40,9 +42,55 @@ export class HandlerToDeleteVM {
         this.buttons.forEach( button => {
             button.addEventListener('click', async (event) => {
                 const userPressedRemove = await this.modalAlert();
-                if(userPressedRemove) this.fetchOnClick(event)
+                if(userPressedRemove) {
+                    this.fetchOnClick(event);
+                    this.handleToSearching.reload();
+                }
             });
         });
+    }
+
+} // End_class
+
+
+export class HandlerToSearching {
+
+    private searcherInput: HTMLInputElement;
+
+    constructor(searcherInputId: string){
+        this.searcherInput = document.querySelector(searcherInputId) as HTMLInputElement;
+    }
+
+    public reload() {
+        location.reload();
+    }
+
+    private filterList = () => {
+        var filterTxt: string;
+        var virtualMachineDiv: HTMLElement;
+        var i: number, txtValue;
+        const li = document.getElementsByClassName('vm-item') as HTMLCollectionOf<HTMLElement>;
+
+        filterTxt = this.searcherInput.value.toUpperCase();
+
+        // Loop through all list items, and hide those who don't match the search query
+        for(i=0; i < li.length; i++) {
+            virtualMachineDiv = li[i].querySelector('.vm-name') as HTMLElement;
+            txtValue = virtualMachineDiv.textContent as string;
+            if(txtValue.toUpperCase().indexOf(filterTxt) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+
+    }
+
+    public addEventListenerForSearcherInput() {
+        this.searcherInput.addEventListener( 'keyup', this.filterList);
+        
+        // Disable spell checker
+        this.searcherInput.spellcheck = false;
     }
 
 } // End_class
--- applications/web/typescript/deleteVirtualMachine/main.ts
+++ applications/web/typescript/deleteVirtualMachine/main.ts
@@ -1,4 +1,7 @@
-import { HandlerToDeleteVM } from "./handler";
+import { HandlerToDeleteVM, HandlerToSearching } from "./handler";
 
+const handlerToSearching = new HandlerToSearching('#searcherInput');
 const handlerToRemoveVirtualMachine = new HandlerToDeleteVM('.container', '.close');
+
 handlerToRemoveVirtualMachine.addEventListeners();
+handlerToSearching.addEventListenerForSearcherInput();
